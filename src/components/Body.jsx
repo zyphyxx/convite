8c9d65cc0891c59e86c7fffff7ba779ff619bb6f
Edgar Jesus <mendesbr92@gmail.com>
2024-02-25 16:46:52 -0300

    feat: aumento de tamanho da img

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IMG } from "../data";
 
 export default function Body() {
+  const [imageSize1, setImageSize1] = useState("200px");
+  const [imageSize2, setImageSize2] = useState("200px");
+  const [imageSize3, setImageSize3] = useState("200px");
+  const [imageSize4, setImageSize4] = useState("200px");
+  const [imageSize5, setImageSize5] = useState("200px");
+
+  const toggleImageSize1 = () => {
+    setImageSize1(imageSize1 === "200px" ? "400px" : "200px");
+  };
+
+  const toggleImageSize2 = () => {
+    setImageSize2(imageSize2 === "200px" ? "400px" : "200px");
+  };
+
+  const toggleImageSize3 = () => {
+    setImageSize3(imageSize3 === "200px" ? "400px" : "200px");
+  };
+
+  const toggleImageSize4 = () => {
+    setImageSize4(imageSize4 === "200px" ? "400px" : "200px");
+  };
+
+  const toggleImageSize5 = () => {
+    setImageSize5(imageSize4 === "200px" ? "400px" : "200px");
+  };
+
   return (
     <div className="text-center mb-8" id='total'>
       <section className="text-center mb-8">
@@ -10,11 +36,10 @@ export default function Body() {
         </div>
       </section>
 
-      <section >
+      <section>
         <p className='text-sky-500'>
-          🎉 Queridos amigos e familiares, estamos muito feliz em convidar vocês para a <b>festinha de 1 ano</b> do nosso pequeno explorador.  🎁 
+          🎉 Queridos amigos e familiares, estamos muito felizes em convidar vocês para a <b>festinha de 1 ano</b> do nosso pequeno explorador.  🎁
         </p>
-      
       </section>
 
       <section className="text-center mb-8">
@@ -25,12 +50,13 @@ export default function Body() {
 
       <section>
         <div>
-          <p className='text-sky-500'> Vamos celebrarmos juntos este momento mágico 🎉</p>
+          <p className='text-sky-500'> Vamos celebrar juntos este momento mágico 🎉</p>
         </div>
       </section>
 
-      <div className="grid justify-items-center mb-8">
-        <img src={IMG[6].image} alt="Imagem" width="200px" className="rounded-lg" />
+      {/* Adicionando manipulador de evento de clique e estilo de tamanho dinâmico */}
+      <div className="grid justify-items-center mb-8" onClick={toggleImageSize1}>
+        <img src={IMG[6].image} alt="Imagem" width={imageSize1} className="rounded-lg" />
       </div>
 
       <section className="text-sky-500 mb-8">
@@ -39,40 +65,41 @@ export default function Body() {
             🦖 Embarque nesta incrível aventura ao mundo dos <b>dinossauros !</b>
           </p>
           <p>
-          Em uma festa cheia de <b>cores vibrantes, brincadeiras</b> e muitas descobertas pré-históricas aguardam por vocês. 🌈
+            Em uma festa cheia de <b>cores vibrantes, brincadeiras</b> e muitas descobertas pré-históricas aguardam por vocês. 🌈
           </p>
         </div>
       </section>
 
-      <div className="grid justify-items-center mb-8">
-        <img src={IMG[8].image} alt="Dinossauro" className="rounded-lg" width="200px" />
+      <div className="grid justify-items-center mb-8" onClick={toggleImageSize2}>
+        <img src={IMG[8].image} alt="Dinossauro" width={imageSize2} className="rounded-lg" />
       </div>
 
       <section className="text-sky-500 mb-8">
         <div className="text-center mb-8">
-          <p >
+          <p>
             Mal podemos esperar para compartilhar este dia tão especial com todos vocês! 🌟
           </p>
 
-          <div className="grid justify-items-center mb-8">
-            <img src={IMG[4].image} alt="" className="rounded-lg" width="200px"  />
+          <div className="grid justify-items-center mb-8" onClick={toggleImageSize3}>
+            <img src={IMG[4].image} alt="" className="rounded-lg" width={imageSize3} />
           </div>
-        
+
           <p className="mb-4">
             Seu carinho é o presente mais valioso, mas se quiserem tornar esta jornada ainda mais inesquecível, ficaremos imensamente gratos! 🎁🌟
           </p>
-          <div className="grid justify-items-center mb-4">
-            <img src={IMG[9].image} width="200px" alt="Leo'saurinho Rex" />
+          <div className="grid justify-items-center mb-4" onClick={toggleImageSize4}>
+            <img src={IMG[9].image} width={imageSize4} alt="Leo'saurinho Rex" />
           </div>
           <p className="mb-4">
             <b>Leo'saurinho Rex</b> está crescendo rápido, e as roupinhas que cabem nele são tamanho 3. Será uma alegria imensa contar com a presença de todos vocês! 🌟
           </p>
         </div>
 
-        <div className="grid justify-items-center mb-4">
-            <img src={IMG[10].image} width="200px" alt="Leo'saurinho Rex" />
-          </div>
+        <div className="grid justify-items-center mb-4" onClick={toggleImageSize5}>
+          <img src={IMG[10].image} width={imageSize5} alt="Leo'saurinho Rex" />
+        </div>
       </section>
+
 
       <section className="text-center mb-8">
         <p className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500"><b>Mal podemos esperar para compartilhar este dia mágico com vocês!</b></p>
